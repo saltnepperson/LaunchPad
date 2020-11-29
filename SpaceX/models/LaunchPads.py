f87a9f1925661ba1518b365c9b092cb6ab5116c6
Tyler Epperson <tyler@Tylers-MacBook-Pro.local>
2020-11-29 00:48:18 -0500

    Group models into separate folder

--- SpaceX/models/LaunchPads.py
+++ SpaceX/models/LaunchPads.py
@@ -1,6 +1,17 @@
+import json
+
 from django.db import models
 
 class LaunchPads(models.Model):
-    id = models.UUIDField()
-    name = models.CharField()
-    status = models.CharField()
+    # id must be a string
+    id = models.CharField(max_length=24, primary_key=True)
+    name = models.CharField(max_length=256)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def all(self):
+        data = LaunchPads.objects.all().values('id','name','status')
+        data = json.dumps(list(data))
+        return data
---
+++ SpaceX/models/__init__.py
@@ -0,0 +1,1 @@
+from .LaunchPads import LaunchPads
