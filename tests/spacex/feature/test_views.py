b50cb06cfa73fb81176902ce8ed9d45663761b31
Tyler Epperson <tyler@Tylers-MacBook-Pro.local>
2020-11-29 14:01:17 -0500

    Feature tests for endpoints

--- tests/spacex/feature/test_views.py
+++ tests/spacex/feature/test_views.py
@@ -3,6 +3,16 @@ from django.urls import reverse
 
 class TestLaunchPadsView(TestCase):
 
-    def test_context(self):
-        response = self.client.get(reverse('get-api-launchpads'))
-        self.assertGreater(len(response.context), 0)
+    def setUp(self):
+        pass
+
+    def test_get_launchpad_data_from_spacex_api(self):
+        response = self.client.get(reverse('spacex:get-api-launchpads'))
+        self.assertEqual(response.status_code, 200)
+        # Cannot assert against hard-coded values since we'll have no idea what the 
+        # values are going to be, instead check that there is data
+        self.assertGreater(len(response.json()['data']), 0)
+
+    def test_get_launchpad_data_from_model(self):
+        response = self.client.get(reverse('spacex:get-database-launchpads'))
+        self.assertEqual(response.status_code, 200)
