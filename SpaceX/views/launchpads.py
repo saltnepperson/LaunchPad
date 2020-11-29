20cc200f3f2d430d9889ec166cd55b79023353dc
Tyler Epperson <tyler@Tylers-MacBook-Pro.local>
2020-11-29 14:57:22 -0500

    Simple logging

--- SpaceX/views/launchpads.py
+++ SpaceX/views/launchpads.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from django.http import JsonResponse
 
@@ -6,11 +7,20 @@ from spacex.spacex_api.entities.launchpads import LaunchPads
 from spacex.adapters.LaunchPadsAdapter import LaunchPadsAdapter
 from spacex.models.LaunchPads import LaunchPads as LaunchPadsModel
 
+logger = logging.getLogger(__name__)
+
 def get_launchpads(request):
-    data = LaunchPadsAdapter(LaunchPads).all()
-    return JsonResponse({"data": data}, status=200)
+    try:
+        data = LaunchPadsAdapter(LaunchPads).all()
+        return JsonResponse({"data": data}, status=200)
+    except Exception as exception:
+        logger.exception(exception)
+
 
 def get_database_launchpads(request):
-    model = LaunchPadsModel()
-    data = model.all()
-    return JsonResponse({"data": data}, status=200)
+    try:
+        model = LaunchPadsModel()
+        data = model.all()
+        return JsonResponse({"data": data}, status=200)
+    except Exception as exception:
+        logger.exception(exception)
