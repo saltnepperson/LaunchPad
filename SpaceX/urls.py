fc6fd834402476ebadf2ed9bc4cb60ea6c53e9b9
Tyler Epperson <tyler@Tylers-MacBook-Pro.local>
2020-11-29 13:59:37 -0500

    Add namespacing for spacex urls

--- SpaceX/urls.py
+++ SpaceX/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 
 from spacex.views import launchpads
 
+app_name = 'spacex'
+
 urlpatterns = [
     path('launchpads', launchpads.get_launchpads, name='get-api-launchpads'),
     path('database/launchpads', launchpads.get_database_launchpads, name='get-database-launchpads')
